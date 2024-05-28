74e8181aae6d239a849de3a07b5b04cd20c8d085
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-28 15:26:25 +0200

    added first object in emaillist

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 
 
+'use strict';
 
+
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            listemail:[],
+            objemail:{},
+
+            
+           
+            
+            
+        }
+    },
+    methods:{
+        axiosGetEmail(){
+            
+            
+            
+        
+        }
+     
+    },
+    mounted(){
+        
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then ((response) => {
+                
+                const result = response.data
+                this.objemail = result
+                this.listemail.push(this.objemail)
+            
+            
+            })
+        
+        
+        
+          
+    }
+    
+}).mount('#app');
 
 
 
